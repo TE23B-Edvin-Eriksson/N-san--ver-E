b0e0446b8ab9f3eac1dbcac25faceb89ebc9dd8f
Edvin Eriksson <edvin.eriksson@elev.ga.ntig.se>
2024-11-19 13:20:52 +0100

    Näsan Över E del 2 fråga 6

---
+++ 02 Listor och arrayer/02 Listor och arrayer/Program.cs
@@ -0,0 +1,47 @@
+﻿
+//1
+//List<string> leksaker = new List<string> { "Teddybjörn", "Lego", "Docka", "Actionfigur", "Radiostyrd bil" };
+
+//2
+
+//List<string> leksaker = new List<string> { "Teddybjörn", "Lego", "Docka", "Actionfigur", "Radiostyrd bil" };
+//
+//foreach (string leksak in leksaker)
+//        {
+//            Console.WriteLine(leksak);
+//        }
+//
+//Console.ReadLine();
+
+
+//3
+//List<string> klasskompisar = new List<string> { "Gustav", "David", "Sandor", "Kevin", "Renat" };
+
+//Console.ReadLine();
+
+//4
+
+//int[] integerArray = { 5, 6, 7, 8, 9 };
+//
+//foreach (int number in integerArray)
+//{
+//    Console.WriteLine(number);
+//}
+//Console.ReadLine();
+
+//5
+// List<int> siffror = [1,2,3,4,5];
+// List<string> namn = ["Sandor", "Renat", "Gustav", "Kevin", "David"];
+// List<string> leksak = ["Teddybjörn", "Lego", "Docka", "Actionfigur", "Radiostyrd bil"];
+// for(int n=0; n<5;)
+// {
+//     Console.WriteLine(namn[n] + " ger " + leksak[n] + " betyget " + siffror[n]);
+//     n++;
+// }
+
+//6
+
+//List<string> cities = [];
+
+
+
